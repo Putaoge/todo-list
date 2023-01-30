3c5cdc04d7b69137fdcda65d1a3114219c287688
Putaoge <86531352+Putaoge@users.noreply.github.com>
2023-01-30 08:58:41 +0800

    Add files via upload

---
+++ part1.js
@@ -0,0 +1,269 @@
+'use strict';
+const options = document.querySelector('.options')
+const optionIcon = document.querySelectorAll('.optionIcon')
+const optionBox = document.querySelectorAll('.optionBox')
+const add = document.querySelector('.add')
+const history = document.querySelector('.history')
+const search = document.querySelector('.search')
+const xuanxiangBox = document.querySelector('.xuanxiangBox')
+const optionList = document.querySelector('.optionList')
+const contentBox = document.querySelector('.contentBox')
+let doneVoice = document.querySelector('.doneVoice')
+let cancelVoice = document.querySelector('.cancelVoice')
+const saveState = {
+    optionListIsOn : false,
+    heightGTwidth : false,
+    widthGTheight : false,
+    addTargetBox: false,
+}
+xuanxiangBox.onclick = ()=>{
+    if (!saveState.optionListIsOn){
+        saveState.optionListIsOn = true
+        xuanxiangBox.style.transform = 'rotate(-90deg)'
+        xuanxiangBox.style.backgroundColor = '#909399'
+        reSizeBox()
+    }else{
+        xuanxiangBox.style.transform = 'rotate(0deg)'
+        xuanxiangBox.style.backgroundColor = 'rgba(255, 255, 255, 0.7)'
+        saveState.optionListIsOn = false
+        reSizeBox()
+    }
+}
+window.onload = ()=>{
+    reSize()
+    showTarget()
+}
+window.onresize=()=>{
+    reSize()
+}
+function reSize(){
+    if (window.innerHeight > window.innerWidth) {
+        if (!saveState.heightGTwidth){
+            saveState.heightGTwidth = true;
+            saveState.widthGTheight = false;
+            reSizeBox()
+        }
+    }else{
+        if (!saveState.widthGTheight){
+            saveState.widthGTheight = true;
+            saveState.heightGTwidth = false
+            reSizeBox()
+        }
+    }
+}
+function reSizeBox(){
+    if (saveState.optionListIsOn == true) {
+        if (window.innerHeight >= window.innerWidth) {
+            optionsBoxSize('8vh')
+            optionsFont('6vh')
+            optionList.style.width = '26vh'
+        } else {
+            optionsBoxSize('6.5vw')
+            optionsFont('5vw')
+            optionList.style.width = '20.5vw'
+        }
+    } else {
+        optionList.style.width = '0px'
+        optionList.style.overflow = 'hidden'
+        if (window.innerHeight >= window.innerWidth) {
+            optionsBoxSize('7vh')
+            optionsFont('5vh')
+        } else {
+            optionsBoxSize('6vw')
+            optionsFont('4vw')
+        }
+    }
+}
+function optionsFont(size){
+    optionIcon.forEach((item) => {
+        item.style.fontSize = size;
+    })
+}
+function optionsBoxSize(size){
+    optionBox.forEach((item) => {
+        item.style.width = item.style.height = item.style.lineHeight =  size;
+    })
+}
+add.addEventListener('click', throttle(()=>{
+    if (saveState.addTargetBox){
+        let addTargetBox = document.querySelector('.addTargetBox')
+        setTimeout(() => {
+            addTargetBox.style.boxShadow = 'none'
+            addTargetBox.style.backgroundColor = '';
+            addTargetBox.style.opacity = '0'
+            contentBox.style.filter = 'blur(0px)'
+            add.style.transform = 'rotate(0)'
+            setTimeout(() => {
+                cover.remove()
+                saveState.addTargetBox = false;
+                add.classList.remove('optionsBoxFocus')
+            }, 300)
+        }, 0)
+        return;
+    }
+    saveState.addTargetBox = true;
+    add.classList.add('optionsBoxFocus')
+    add.style.transform = 'rotate(-45deg)'
+    const div = document.createElement('div')
+    div.classList.add('addTarget')
+    const addTargetBox = document.createElement('div')
+    addTargetBox.classList.add('addTargetBox')
+    div.innerHTML = `
+        <h2>Add Target</h2>
+            <div class="addInput">
+                <input class="targetValue" type="text" placeholder="事項" >
+            </div>
+            <div class="targetType">
+                <select name="pets" class="typeList">
+                    <option disabled value="">請選擇事件類型</option>
+                    <option value="work">工作</option>
+                    <option value="study">學習</option>
+                    <option value="life" selected>生活</option>
+                    <option value="relationship">感情</option>
+                    <option value="Chill">休閒</option>
+                    <option value="urgent">緊急</option>
+                </select>
+            </div>
+            <div class="submitBox">
+                send
+            </div>
+            <div class="addExitBox">
+                exit
+            </div>
+    `
+    div.style.opacity = '0';
+    let cover = createCover()
+    document.body.append(cover)
+    cover.append(addTargetBox)
+    addTargetBox.append(div)
+    contentBox.style.filter = 'blur(5px)'
+    setTimeout(() => {
+        addTargetBox.style.backgroundColor = 'white';
+        addTargetBox.style.boxShadow = '0 3px 5px 1px black';
+        div.style.opacity = '1'
+    }, 0)
+    let targetValue = document.querySelector('.targetValue')
+    const submitBtn = document.querySelector('.submitBox')
+    const addExitBox = document.querySelector('.addExitBox')
+    addExitBox.onclick = throttle(()=>{
+            addTargetBox.style.boxShadow = 'none'
+            addTargetBox.style.backgroundColor = '';
+            div.style.opacity = '0'
+            contentBox.style.filter = 'blur(0px)'
+            add.style.transform = 'rotate(0)'
+            add.classList.remove('optionsBoxFocus')
+            setTimeout(() => {
+                cover.remove()
+                saveState.addTargetBox = false;
+                add.style.transform = 'rotate(0)'
+                add.classList.remove('optionsBoxFocus')
+            }, 300)
+    },300)
+    submitBtn.onclick = throttle((event) => {
+        if (!targetValue.value) {
+            return
+        }
+        addTargetBox.style.filter = 'blur(5px)'
+        if (localStorage.getItem('Target')) {
+            let previousTarget = JSON.parse(localStorage.getItem('Target'))
+            let length = previousTarget.length
+            const newTarget = getTarget(length)
+            previousTarget[length] = newTarget
+            localStorage.setItem('Target', JSON.stringify(previousTarget))
+        } else {
+            const newTarget = []
+            newTarget.push(getTarget(0))
+            localStorage.setItem('Target', JSON.stringify(newTarget))
+        }
+        sendSuccess()
+        function removeDiv(){
+            setTimeout(() => {
+                addTargetBox.style.boxShadow = 'none'
+                addTargetBox.style.backgroundColor = '';
+                div.style.opacity = '0'
+                contentBox.style.filter = 'blur(0px)'
+                add.style.transform = 'rotate(0)'
+                add.classList.remove('optionsBoxFocus')
+                cover.remove()
+                setTimeout(()=>{
+                    saveState.addTargetBox = false;
+                }, 300)
+            }, 1000)
+        }
+        removeDiv()
+    },300)
+}, 300))
+function getTarget(index) {
+    let targetValue = document.querySelector('.targetValue')
+    let target = {}
+    const type = getType()
+    let value = { value: targetValue.value }
+    const done = { done: false }
+    const time = getTime()
+    let rank = { rank: index }
+    target = Object.assign(value, type, time, done, rank)
+    return target
+}
+function getType() {
+    const typeList = document.querySelector('.typeList')
+    const index = typeList.selectedIndex
+    return { type: typeList[index].value }
+}
+function getTime(){
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = now.getMonth() + 1
+    let day = now.getDate()
+    day = day < 10 ? '0' + day : day
+    let hour = now.getHours()
+    hour = hour < 10 ? '0' + hour : hour
+    let minute = now.getMinutes()
+    minute = minute < 10 ? '0' + minute : minute
+    const ms = now.getTime()
+    const time = `${year}年${month}月${day}日${hour}時${minute}分`
+    return { time: [time, ms]}
+}
+function sendSuccess(){
+    let cover = createCover()
+    let div = document.createElement('div')
+    Object.assign(div.style,{
+        position:'fixed',
+        bottom: '40vh',
+        width: '100vw',
+        height: '20vh',
+        backgroundColor:'#2C3532',
+        transition: 'all .3s',
+        overflow:'hidden',
+        textAlign: 'center',
+        lineHeight: '20vh',
+        color:'#fff',
+        fontSize:'40px',
+        opacity:'0',
+    })
+    div.textContent = '提交成功'
+    div.style.userSelect = 'none'
+    document.body.append(cover)
+    cover.append(div)
+    setTimeout(()=>{
+        div.style.opacity = '.9'
+        setTimeout(() => {
+            div.style.opacity = '0'
+            setTimeout(() => {
+                cover.remove()
+            }, 300)
+        }, 1000)
+    }, 0)
+    showTarget()
+}
+function throttle(fn, delay) {
+    let timer = null
+    return function () {
+        if (timer) {
+            return
+        }
+        fn()
+        timer = setTimeout(() => {
+            timer = null
+        }, delay)
+    }
+}
---
+++ part2.js
@@ -0,0 +1,186 @@
+function showTarget(){
+    if (document.querySelector('.contentList')){
+        document.querySelector('.contentList').remove()
+    }
+    let div = document.createElement('div')
+    if(!localStorage.getItem('Target')){
+        div.classList.add('noContent')
+        let p = document.createElement('p')
+        p.textContent = 'no list yet'
+        div.append(p)
+
+        contentBox.append(div)
+        return 
+    }
+
+    let ul = document.createElement('ul')
+    ul.classList.add('contentList')
+    let Target = getTargetInfo().reverse()
+    let tempStr = ``
+    let tempArr = []
+    Target.forEach((item, index)=>{
+        if(item.done === true){
+            tempArr.push(index)
+        }
+        let text = 
+        `<li class="contentItem">
+                <div class="targetText">
+                    <p class="date">${item.time[0]}</p>
+                    <p class="text">${item.value}</p>
+                </div>
+                <span 
+                class="iconfont icon-icon chooseMenu doneOrFalse-${item.rank}"></span>
+            </li>`  
+        tempStr += text
+    })
+    ul.innerHTML = tempStr
+    tempArr.forEach(item=>{
+        setDoneState(ul.childNodes[item], Target[item].costTime)
+    })
+    
+    
+
+    
+    contentBox.append(ul)
+    setMenuEvent()
+}
+
+function getTargetInfo(){
+    if (!localStorage.getItem('Target')){
+        return
+    }
+    return JSON.parse(localStorage.getItem('Target'))
+}
+function setMenuEvent(){
+    let aChooseMenu = document.querySelectorAll('.chooseMenu')
+    aChooseMenu.forEach(item=>{
+        item.onclick = (event)=>{
+            createOptionsWindow(event)
+        }
+    })
+}
+function createOptionsWindow(event){
+    let contentItem = event.target.parentNode
+    let cover = createCover()
+    cover.innerHTML = 
+    `
+        <div class="DoneOrCancelBox">
+            <div class="doneKey">
+                <span class="iconfont icon-wancheng"></span>
+                完成
+            </div>
+            <div class="deleteKey">
+                <span class="iconfont icon-quxiao"></span>
+                刪除
+            </div>
+            <span class="iconfont icon-quxiao1"></span>
+        </div>
+    `
+    document.body.append(cover)
+    contentBox.style.filter = 'blur(5px)'
+    let cancel =  document.querySelector('.icon-quxiao1')
+    let deleteKey = document.querySelector('.deleteKey')
+    let doneKey = document.querySelector('.doneKey')
+    let rank = event.target.classList[3].split('-')[1]
+    let targetInfo = getTargetInfo()
+    let index;
+    for (let i = 0; i < targetInfo.length; i++){
+        if (targetInfo[i].rank == rank){
+            index = i
+        }
+    }
+    cancel.onclick = (event)=>{
+        exit()
+    }
+    deleteKey.onclick = (event)=>{
+        exit()
+        cancelVoice.play()
+        delTarget(contentItem, index)
+    }
+    doneKey.onclick = (event)=>{
+        exit()
+        doneVoice.play()
+        let costTime = getDiffTime(targetInfo[rank].time[1])
+        setDoneState(contentItem, costTime)
+        targetInfo[rank].done = true
+        targetInfo[rank].costTime = costTime
+        localStorage.setItem('Target', JSON.stringify(targetInfo))
+    }
+    function exit(){
+        cover.remove()
+        contentBox.style.filter = 'blur(0px)'
+    }
+}
+function delTarget(contentItem, index){
+    contentItem.classList.add('leaveMove')
+    setTimeout(()=>{
+        contentItem.remove()
+        delTargetInfo(index)
+    },1000)
+}
+function delTargetInfo(index){
+    let target = getTargetInfo()
+    target.splice(index,1)
+    localStorage.setItem('Target', JSON.stringify(target))
+}
+function setDoneState(contentItem, costTime){
+    contentItem.classList.add('successTarget')
+    let span = document.createElement('span')
+    span.className = "iconfont icon-wancheng1"
+    Object.assign(span.style,{
+        position: 'absolute',
+        right: '10px',
+        bottom: '0px',
+        userSelect : 'none',
+    })
+    let p = document.createElement('p')
+    p.textContent = `用時: ${costTime}`
+    p.classList.add('costTime')
+    contentItem.append(span,p)
+}
+function createCover(){
+    let div = document.createElement('div')
+    div.classList.add('cover')
+    return div
+}
+function getDiffTime(previousTime){
+    let now = +new Date()
+    let diff = now - previousTime
+    let msOneDay = 1000 * 60 * 60 * 24
+    let msOneHour = 1000 * 60 * 60
+    let msOneMinute = 1000 * 60
+    let msOneSecond = 1000
+    let result = ``;
+    let Days, Hours, Minutes, Seconds
+    if(diff > msOneDay){
+        Days = ~~(diff / msOneDay)
+        diff -= Days * msOneDay
+        Hours = ~~(diff / msOneHour)
+        diff -= Hours * msOneHour
+        Minutes = ~~(diff / msOneMinute)
+        diff -= Minutes * msOneMinute
+        Seconds = ~~(diff / msOneSecond)
+        result = `${Days}天 ${Hours}小時 ${Minutes}分鐘 ${Seconds}秒`
+    }else if(diff > msOneHour){
+        Hours = ~~(diff / msOneHour)
+        diff -= Hours * msOneHour
+        Minutes = ~~(diff / msOneMinute)
+        diff -= Minutes * msOneMinute
+        Seconds = ~~(diff / msOneSecond)
+        result = `${Hours}小時 ${Minutes}分鐘 ${Seconds}秒`
+    }else if(diff>msOneMinute){
+        Minutes = ~~(diff / msOneMinute)
+        diff -= Minutes * msOneMinute
+        Seconds = ~~(diff / msOneSecond)
+        result = `${Minutes}分鐘${Seconds}秒`
+    } else if (diff > msOneSecond){
+        Seconds = ~~(diff / msOneSecond)
+        result = `${Seconds}秒`
+    }else if(diff){
+        result = `你好, 穿越者`
+    }else{
+        result = `太快啦`
+    }
+    return result
+}
+let fakeTime = +new Date('2023/01/31')
